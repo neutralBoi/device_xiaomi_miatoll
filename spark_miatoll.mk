d6af080e484f11e0e850a3b5d0f2032e3cbaa582
NeutralBoi <vedantshirsekar92@gmail.com>
2022-05-08 06:04:23 +0200

    Miatoll: SparkOS Bringup

--- spark_miatoll.mk
+++ spark_miatoll.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/spark/common_full_phone.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
